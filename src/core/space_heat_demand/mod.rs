dfc33836ac5fb97e33eaba9a6337ae6c3c954455
kpinakula <7438240+kpinakula@users.noreply.github.com>
2024-08-07 15:15:08 +0100

    Create new ventilation module from 0.30
    Ticket: EC-69
    
    This commit creates a new `ventilation.rs` module and implements the
    first few functions within it. More to come..
    
    Co-Authored-By: Douglas Greenshields <douglas.greenshields@levellingup.gov.uk>

--- src/core/space_heat_demand/mod.rs
+++ src/core/space_heat_demand/mod.rs
@@ -1,5 +1,6 @@
 pub mod building_element;
 pub mod internal_gains;
 pub mod thermal_bridge;
+mod ventilation;
 pub mod ventilation_element;
 pub mod zone;
---
+++ src/core/space_heat_demand/ventilation.rs
@@ -0,0 +1,64 @@
+// This module provides objects to represent Infiltration and Ventilation.
+// The calculations are based on Method 1 of BS EN 16798-7.
+
+use crate::core::material_properties::AIR;
+use crate::core::units::SECONDS_PER_HOUR;
+
+fn p_a_ref() -> f64 {
+    AIR.density_kg_per_m3()
+}
+fn c_a() -> f64 {
+    AIR.specific_heat_capacity_kwh()
+}
+
+// (Default values from BS EN 16798-7, Table 11)
+// Coefficient to take into account stack effect in airing calculation in (m/s)/(m*K)
+const C_STACK: f64 = 0.0035;
+// Coefficient to take into account wind speed in airing calculation in 1/(m/s)
+const C_WND: f64 = 0.001;
+// Gravitational constant in m/s2
+const G: f64 = 9.81;
+//Room temperature in degrees K
+const T_E_REF: f64 = 293.15;
+//Absolute zero in degrees K
+const T_0_ABS: f64 = 273.15;
+
+/// Calculate pressure difference between the exterior and the interior of the dwelling
+/// for a flow path (at it's elevavation above the vent zone floor)
+/// Arguments:
+/// * `h_path` - height of air flow path (m)
+/// * `c_p_path` - wind pressure coefficient
+/// * `u_site` - wind velocity at zone level (m/s)
+/// * `t_e` - external air temperature (K)
+/// * `t_z` - thermal zone air temperature (K)
+/// * `p_z_ref` - internal reference pressure (Pa)
+///
+fn calculate_pressure_difference_at_an_airflow_path(
+    h_path: f64,
+    c_p_path: f64,
+    u_site: f64,
+    t_e: f64,
+    t_z: f64,
+    p_z_ref: f64,
+) -> f64 {
+    let p_e_path = p_a_ref() * T_E_REF / t_e * (0.5 * c_p_path * u_site.powi(2) - h_path * G); //(5)
+    let p_z_path = p_z_ref - p_a_ref() * h_path * G * T_E_REF / t_z; //(6)
+    p_e_path - p_z_path //(4)
+}
+
+/// Convert infiltration rate from ach to m^3/s
+fn air_change_rate_to_flow_rate(air_change_rate: f64, zone_volume: f64) -> f64 {
+    air_change_rate * zone_volume / f64::from(SECONDS_PER_HOUR)
+}
+
+// TODO:
+// WIP: a bunch of top level functions (called from other parts of ventilation.py)
+// a Window class
+// a WindowPart class
+// a Vent class
+// a Leaks class
+// a AirTerminalDevices class
+// a Cowls class
+// a CombustionAppliances class
+// a MechanicalVentilation class
+// InfiltrationVentilation class
