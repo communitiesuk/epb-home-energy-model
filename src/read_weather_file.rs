be870a6b9273c7b2fc5587297a69d1d88f14205e
Russ Penska <russpenska@mt-mbp-l4c961tvv6.home>
2024-09-03 09:06:12 +0100

    include wind_directinos from weather file in ExternalConditions

--- src/read_weather_file.rs
+++ src/read_weather_file.rs
@@ -5,6 +5,7 @@ const COLUMN_LONGITUDE: usize = 7;
 const COLUMN_LATITUDE: usize = 6;
 const COLUMN_AIR_TEMP: usize = 6; // dry bulb temp in degrees
 const COLUMN_WIND_SPEED: usize = 21; // wind speed in m/sec
+const COLUMN_WIND_DIRECTION: usize = 20; // wind direction in degrees
 const COLUMN_DNI_RAD: usize = 14; // direct beam normal irradiation in Wh/m2
 const COLUMN_DIF_RAD: usize = 15; // diffuse irradiation (horizontal plane) in Wh/m2
 const _COLUMN_GROUND_REFLECT: usize = 32;
@@ -13,6 +14,7 @@ const _COLUMN_GROUND_REFLECT: usize = 32;
 pub struct ExternalConditions {
     pub air_temperatures: Vec<f64>,
     pub wind_speeds: Vec<f64>,
+    pub wind_directions: Vec<f64>,
     pub diffuse_horizontal_radiation: Vec<f64>,
     pub direct_beam_radiation: Vec<f64>,
     pub solar_reflectivity_of_ground: Vec<f64>,
@@ -29,6 +31,7 @@ pub fn weather_data_to_vec(file: impl Read) -> Result<ExternalConditions, &'stat
 
     let mut air_temperatures = vec![];
     let mut wind_speeds = vec![];
+    let mut wind_directions = vec![];
     let mut diff_hor_rad = vec![];
     let mut dir_beam_rad = vec![];
     let mut ground_solar_reflc = vec![];
@@ -43,6 +46,7 @@ pub fn weather_data_to_vec(file: impl Read) -> Result<ExternalConditions, &'stat
         } else if i >= 8 {
             air_temperatures.push(record.get(COLUMN_AIR_TEMP).unwrap().parse().unwrap());
             wind_speeds.push(record.get(COLUMN_WIND_SPEED).unwrap().parse().unwrap());
+            wind_directions.push(record.get(COLUMN_WIND_DIRECTION).unwrap().parse().unwrap());
             dir_beam_rad.push(record.get(COLUMN_DNI_RAD).unwrap().parse().unwrap());
             diff_hor_rad.push(record.get(COLUMN_DIF_RAD).unwrap().parse().unwrap());
             ground_solar_reflc.push(0.2);
@@ -52,6 +56,7 @@ pub fn weather_data_to_vec(file: impl Read) -> Result<ExternalConditions, &'stat
     Ok(ExternalConditions {
         air_temperatures,
         wind_speeds,
+        wind_directions,
         diffuse_horizontal_radiation: diff_hor_rad,
         direct_beam_radiation: dir_beam_rad,
         solar_reflectivity_of_ground: ground_solar_reflc,
