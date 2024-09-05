7f9033d6a94d9368720f41f8524e8d50c289bfd9
kpinakula <7438240+kpinakula@users.noreply.github.com>
2024-09-05 18:16:03 +0100

    WIP: Add test_calc_auxiliary_energy to HeatBattery
    Note, Python's equivalent test has an assertion using a spy. The way we
    have currently translated changes our implementation. We need to discuss
     as a team if we like this approach / if it is valuable.

--- src/core/heating_systems/heat_battery.rs
+++ src/core/heating_systems/heat_battery.rs
@@ -533,7 +533,7 @@ impl HeatBattery {
         _timestep: f64,
         time_remaining_current_timestep: f64,
         timestep_idx: usize,
-    ) {
+    ) -> f64 {
         // Energy used by circulation pump
         let mut energy_aux = self.total_time_running_current_timestep * self.power_circ_pump;
 
@@ -543,6 +543,7 @@ impl HeatBattery {
         self.energy_supply_connection
             .demand_energy(energy_aux, timestep_idx)
             .unwrap();
+        energy_aux // TODO: decide if it is okay to return this just for a test (`test_calc_auxiliary_energy`) - it used to return nothing/()
     }
 
     /// Calculations to be done at the end of each timestep
@@ -1106,5 +1107,31 @@ mod tests {
 
             heat_battery.lock().timestep_end(t_idx);
         }
+    }
+
+    #[rstest]
+    /// Check heat battery auxilary energy consumption
+    fn test_calc_auxiliary_energy(
+        simulation_time_iterator: Arc<SimulationTimeIterator>,
+        battery_control_on: Control,
+    ) {
+        let heat_battery =
+            create_heat_battery(simulation_time_iterator.clone(), battery_control_on);
+
+        let energy_aux = heat_battery.lock().calc_auxiliary_energy(
+            1.0,
+            0.5,
+            simulation_time_iterator.current_index(),
+        );
+
+        // TODO decide what to do with this test
+        // Python asserts `calc_auxiliary_energy` calls `demand_energy` with the result of a calculation using 3 instance variables (total_time_running_current_timestep, power_circ_pump, power_standby)
+        // Option 1: have `calc_auxiliary_energy` return `energy_aux` (currently implemented)
+        // Option 2: add an `energy_aux` field to the HeatBattery struct and have `calc_auxiliary_energy` set it
+        // Option 3: do something similar but for the supply conntection (e.g. set an `energy_demanded` field)
+        // Option 4: skip this test
+        // Option 5: ??
+        // assert_eq!(heat_battery.lock().energy_aux, 0.0122);
+        assert_eq!(energy_aux, 0.0122); // got 0.0122 from running test in debug mode in Python
     }
 }
