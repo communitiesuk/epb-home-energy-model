03e40a92cb03195e6ec225c8f8e3ee3d1f613764
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2026-01-19 12:34:41 +0000

    implement simple water supply mock variant for test builds only

--- src/core/common.rs
+++ src/core/common.rs
@@ -12,6 +12,8 @@ pub(crate) enum WaterSupply {
     ColdWaterSource(Arc<ColdWaterSource>),
     Wwhrs(Arc<Mutex<Wwhrs>>),
     Preheated(HotWaterStorageTank),
+    #[cfg(test)]
+    Mock(MockWaterSupply),
 }
 
 impl WaterSupply {
@@ -32,6 +34,8 @@ impl WaterSupply {
                     Ok(rw_lock.read().get_temp_cold_water(volume_needed))
                 }
             },
+            #[cfg(test)]
+            WaterSupply::Mock(mock) => Ok(mock.get_temp_cold_water(volume_needed)),
             _ => unimplemented!("TODO during migration 1.0.0a1"),
         }
     }
@@ -53,7 +57,30 @@ impl WaterSupply {
                     rw_lock.read().draw_off_water(volume_needed, simtime)
                 }
             },
+            #[cfg(test)]
+            WaterSupply::Mock(mock) => Ok(mock.draw_off_water(volume_needed)),
             _ => unimplemented!("TODO during migration 1.0.0a1"),
         }
+    }
+}
+
+#[cfg(test)]
+#[derive(Clone, Copy, Debug)]
+pub(crate) struct MockWaterSupply {
+    temperature: f64,
+}
+
+#[cfg(test)]
+impl MockWaterSupply {
+    pub(crate) fn new(temperature: f64) -> Self {
+        Self { temperature }
+    }
+
+    pub(crate) fn get_temp_cold_water(&self, volume_needed: f64) -> Vec<(f64, f64)> {
+        vec![(self.temperature, volume_needed)]
+    }
+
+    pub(crate) fn draw_off_water(&self, volume_needed: f64) -> Vec<(f64, f64)> {
+        self.get_temp_cold_water(volume_needed)
     }
 }
