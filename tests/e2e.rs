a50983f52bcef92600c46169911ffe3f02639a13
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2026-05-29 17:48:34 +0100

    e2e tests: add comparison of tabular files

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -36,6 +36,13 @@ fn files() -> Vec<DirEntry> {
 // there is one short file that is treated differently to the rest in the e2e tests, i.e. it is given different parameters for a calculation run
 const EXCEPTIONAL_DEMO_FILE: &str = "demo_hp_with_setback_separate_ieh_plus_cooling";
 
+const NON_TABULAR_FILE_SUFFIXES: &[&str] = &[
+    "results_summary.csv",
+    "results_static.csv",
+    "postproc.csv",
+    "postproc_summary.csv",
+];
+
 #[rstest]
 fn test_run_all_files(files: Vec<DirEntry>) {
     let london_weather = cibse_weather_data_to_external_conditions(BufReader::new(Cursor::new(
@@ -43,7 +50,8 @@ fn test_run_all_files(files: Vec<DirEntry>) {
     )))
     .unwrap();
 
-    files.par_iter().for_each(move |file| {
+    let difference_count: usize = files.par_iter().map(move |file| {
+        let mut difference_count = 0usize;
         let output_writer = InMemoryDirectoryOutputWriter::new(file.file_name().to_str().unwrap());
         let use_additional_options = use_additional_options(file);
         let result = run_project_from_input_file(
@@ -69,22 +77,35 @@ fn test_run_all_files(files: Vec<DirEntry>) {
             .map(|f| (f.file_name().to_str().unwrap().to_owned(), f))
             .collect();
         let expected_file_count = expected_files.len();
-        for (actual_file_name, actual_file) in output_files.iter() {
-            let expected_file = expected_files.get(actual_file_name);
+        for (file_name, actual_file) in output_files.iter() {
+            let expected_file = expected_files.get(file_name);
             let expected_file = if let Some(expected_file) = expected_file {
                 expected_file
             } else {
-                println!("🐙 Unexpected file emitted: {}", actual_file_name);
+                println!("🐙 Unexpected file emitted: {}", file_name);
                 continue;
             };
             let mut rust_file_read = BufReader::new(Cursor::new(actual_file));
             let mut rust_reader = csv_access::csv_reader(&mut rust_file_read);
             let mut python_file_read = BufReader::new(File::open(expected_file.path()).unwrap());
             let mut python_reader = csv_access::csv_reader(&mut python_file_read);
-            let differences = compare::compare_headers(&mut python_reader, &mut rust_reader);
-            if let Err(differences) = differences {
-                println!("❌ Headers differ for file: {}", actual_file_name);
+            let header_differences = compare::compare_headers(&mut python_reader, &mut rust_reader);
+            if let Err(differences) = header_differences {
+                println!("❌ Headers differ for file: {}", file_name);
                 println!("Differences: {}", differences.iter().join("\n"));
+                difference_count += differences.len();
+            }
+            if is_tabular(file_name) {
+                let file_differences = compare::compare_tabular_records_within_threshold(&mut python_reader, &mut rust_reader);
+                if let Err(comparison_error) = file_differences {
+                    let file_difference_count = comparison_error.differences.len();
+                    println!("❌ Tabular records differ for file: {} - difference count is {}", file_name, file_difference_count);
+                    difference_count += file_difference_count;
+                } else {
+                    println!("✅ Tabular records match for file: {}", file_name);
+                }
+            } else {
+                // TODO: perform non-tabular comparison
             }
         }
         println!(
@@ -103,7 +124,15 @@ fn test_run_all_files(files: Vec<DirEntry>) {
                 "".into()
             },
         );
-    });
+
+        difference_count
+    }).sum();
+
+    assert_eq!(
+        difference_count, 0,
+        "Total difference count: {}",
+        difference_count
+    );
 
     fn use_additional_options(file: &DirEntry) -> bool {
         !file
@@ -159,6 +188,12 @@ fn test_run_all_files(files: Vec<DirEntry>) {
             },
         )
         .to_str()
+    }
+
+    fn is_tabular(file_name: &str) -> bool {
+        !NON_TABULAR_FILE_SUFFIXES
+            .iter()
+            .any(|suffix| file_name.ends_with(suffix))
     }
 
     fn expected_directory(file: &DirEntry) -> WalkDir {
@@ -256,6 +291,7 @@ mod compare {
     use std::fmt;
     use std::io::Read;
     use std::mem::discriminant;
+    use thiserror::Error;
 
     pub fn compare(left: StringRecord, right: StringRecord) -> ComparisonResult {
         OutputRecord::from(left).equiv(&OutputRecord::from(right))
@@ -279,6 +315,16 @@ mod compare {
         RecordDifference {
             message: String,
         },
+    }
+
+    impl Difference {
+        fn field_index(&self) -> Option<usize> {
+            match self {
+                Difference::StringDifference { field_index, .. } => Some(*field_index),
+                Difference::NumberDifference { field_index, .. } => Some(*field_index),
+                Difference::RecordDifference { .. } => None,
+            }
+        }
     }
 
     impl fmt::Display for Difference {
@@ -327,6 +373,26 @@ mod compare {
     }
 
     type ComparisonResult = Result<(), Vec<Difference>>;
+
+    #[derive(Debug)]
+    #[allow(dead_code)]
+    pub struct DifferenceInFile {
+        pub line_number: usize,
+        pub column_field: Option<String>,
+        pub difference: Difference,
+    }
+
+    impl DifferenceInFile {
+        fn new(difference: Difference, line_number: usize, column_field: Option<String>) -> Self {
+            Self {
+                difference,
+                line_number,
+                column_field,
+            }
+        }
+    }
+
+    type FileComparisonResult = Result<(), FileComparisonError>;
 
     impl OutputRecord {
         pub fn equiv(&self, other: &OutputRecord) -> ComparisonResult {
@@ -450,5 +516,87 @@ mod compare {
             .expect("Failed to read Python CSV headers");
 
         compare(py_headers.clone(), rust_headers.clone())
+    }
+
+    pub fn compare_tabular_records_within_threshold<T, U>(
+        py_reader: &mut Reader<T>,
+        rust_reader: &mut Reader<U>,
+    ) -> FileComparisonResult
+    where
+        T: Read,
+        U: Read,
+    {
+        let headers;
+        {
+            // read headers in own scope so we can re-use py_reader later
+            let python_headers = py_reader.headers().expect("Failed to read Python headers");
+            headers = python_headers.clone();
+        }
+
+        let mut rust_records = rust_reader.records().enumerate();
+        let python_records = py_reader.records().enumerate();
+
+        let mut file_differences: Vec<DifferenceInFile> = vec![];
+
+        let mut warnings: Vec<String> = vec![];
+
+        for (record_index, python_record) in python_records {
+            let rust_record = if let Some((_, rust_record)) = rust_records.next() {
+                rust_record
+            } else {
+                warnings.push(format!(
+                    "Rust records are missing from index {}",
+                    record_index
+                ));
+                break;
+            };
+
+            if let Err(ref rust_record) = &rust_record {
+                warnings.push(format!(
+                    "Failed to parse Rust record at index {}: {}",
+                    record_index, rust_record
+                ));
+            }
+            if let Err(ref python_record) = &python_record {
+                warnings.push(format!(
+                    "Failed to parse Python record at index {}: {}",
+                    record_index, python_record
+                ));
+            }
+
+            let (rust_record, python_record) =
+                if let (Ok(rust_record), Ok(python_record)) = (rust_record, python_record) {
+                    (rust_record, python_record)
+                } else {
+                    continue;
+                };
+
+            if let Err(differences) = compare(python_record, rust_record) {
+                file_differences.extend(differences.into_iter().map(|difference: Difference| {
+                    let column_field = difference
+                        .field_index()
+                        .and_then(|index| headers.get(index))
+                        .map(ToOwned::to_owned);
+
+                    DifferenceInFile::new(difference, record_index + 2, column_field)
+                }));
+            }
+        }
+
+        if file_differences.is_empty() {
+            Ok(())
+        } else {
+            Err(FileComparisonError {
+                differences: file_differences,
+                warnings,
+            })
+        }
+    }
+
+    #[derive(Debug, Error)]
+    #[error("File comparison failed with {} differences and {} warnings", self.differences.len(), self.warnings.len())]
+    pub struct FileComparisonError {
+        pub differences: Vec<DifferenceInFile>,
+        pub warnings: Vec<String>,
     }
 }
