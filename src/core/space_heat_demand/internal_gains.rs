13eab69242aab32af9022634f50c4fec64f9d799
kpinakula <kpinakula@users.noreply.github.com>
2026-06-04 18:08:27 +0000

    Fix test_process_event test set up

--- src/core/space_heat_demand/internal_gains.rs
+++ src/core/space_heat_demand/internal_gains.rs
@@ -653,8 +653,8 @@ mod tests {
             energy_supply: &Arc<RwLock<EnergySupply>>,
             simtime: SimulationTime,
         ) -> Arc<SmartApplianceControl> {
-            // following is provided in an invalid place in the Python fixture, so providing
-            // power timeseries data separately here as data structure matters in the Rust
+            // the following is provided in an invalid place in the Python fixture (power_timeseries in appliance_data),
+            // so providing power timeseries data separately here as data structure matters in the Rust
             let power_timeseries = vec![
                 77.70823134533667,
                 70.07710122045972,
@@ -761,9 +761,122 @@ mod tests {
         }
 
         #[fixture]
-        fn event_appliance_gains(
-            simtime: SimulationTime,
+        fn smart_control_for_process_event(
             appliance_data: ApplianceGainsDetails,
+            energy_supply: &Arc<RwLock<EnergySupply>>,
+            simtime: SimulationTime,
+        ) -> Arc<SmartApplianceControl> {
+            // using custom fixture with adjusted power timeseries to match the side effects
+            // that happen to the smart control during the test set up in the Python
+            let power_timeseries = vec![
+                77.70823134533667,
+                70.07710122045972,
+                66.26153469022015,
+                871.9959681599806,
+                823.2054045653432,
+                66.26153469022015,
+                81.52379787557622,
+                872.7293737820189,
+                448.95976141145366,
+                146.38841421163792,
+                779.8539807418776,
+                965.7329037433917,
+                157.8351108667544,
+                146.38841421163792,
+                150.20398074187744,
+                236.48451946096566,
+                1235.1378596577206,
+                257.03982010376774,
+                801.2313187689566,
+                207.4374669530622,
+                192.17520376770614,
+                290.41445627425867,
+                138.757284086761,
+                100.60162759117186,
+                77.70823134533667,
+            ];
+            let non_appliance_demand_24hr = IndexMap::from([(
+                "mains elec".into(),
+                vec![
+                    0.06830825101566576,
+                    0.060105811973985415,
+                    0.05305939286989522,
+                    0.0501236916686892,
+                    0.011659722362322093,
+                    0.009841650327447332,
+                    0.008628179127672608,
+                    0.00780480411138585,
+                    0.007342238555297734,
+                    0.0068683142767418555,
+                    0.007092339066083696,
+                    0.0073738789491060025,
+                    0.00890318157456338,
+                    0.013196350717229778,
+                    3.8185258665440713,
+                    3.686604856404327,
+                    3.321741503132428,
+                    2.1009771847288543,
+                    1.9577604381160962,
+                    0.982853996628817,
+                    -0.4690062980892011,
+                    -0.47106808673125095,
+                    -0.440083286258449,
+                    -0.4402744803667663,
+                    -0.275893537706527,
+                    -0.27582574287859735,
+                    -0.02364598193865506,
+                    -0.022770656131003677,
+                    0.006386180325667274,
+                    1.1838622352604393,
+                    0.016880847238056107,
+                    0.022939243503258204,
+                    0.03451315625040322,
+                    3.6710272517570663,
+                    3.4183577199797917,
+                    3.259661970488036,
+                    2.382744591886866,
+                    3.347517299485186,
+                    2.8633293436146374,
+                    2.194481295688944,
+                    2.0245859635409174,
+                    2.160933115928409,
+                    2.1559541166936143,
+                    2.078707457615306,
+                    0.06082872713634447,
+                    0.05498437799409048,
+                    0.04615437212925211,
+                    0.036699730049032445,
+                ],
+            )]);
+            let battery24hr: SmartApplianceBattery = serde_json::from_value(serde_json::json!({
+                "battery_state_of_charge": {
+                    "mains elec": [
+                                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0
+                    ]
+                }
+            }))
+            .unwrap();
+
+            SmartApplianceControl::new(
+                &IndexMap::from([("mains elec".into(), power_timeseries)]),
+                appliance_data.time_series_step,
+                &simtime.iter(),
+                non_appliance_demand_24hr,
+                battery24hr,
+                &IndexMap::from([("mains elec".into(), energy_supply.clone())]),
+                vec!["Clothes_drying".into()],
+            )
+            .unwrap()
+            .into()
+        }
+
+        #[fixture]
+        fn event_appliance_gains(
+            appliance_data: ApplianceGainsDetails,
+            simtime: SimulationTime,
             smart_control: &Arc<SmartApplianceControl>,
             total_floor_area: f64,
             energy_supply_connection: &EnergySupplyConnection,
@@ -779,13 +892,12 @@ mod tests {
         }
 
         #[rstest]
-        #[ignore = "test would run OK but behaviour within smart control varies from Python - fix this"]
         fn test_process_event(
             mut appliance_data: ApplianceGainsDetails,
             energy_supply_connection: &EnergySupplyConnection,
             simtime: SimulationTime,
             total_floor_area: f64,
-            smart_control: &Arc<SmartApplianceControl>,
+            smart_control_for_process_event: Arc<SmartApplianceControl>,
         ) {
             let event = ApplianceGainsEvent {
                 start: 3.,
@@ -793,22 +905,13 @@ mod tests {
                 demand_w: 900.0,
             };
 
-            // initially create an EventApplianceGains struct to replicate behaviour in Python test
-            let _event_appliance_gains = event_appliance_gains(
-                simtime,
-                appliance_data.clone(),
-                smart_control,
-                total_floor_area,
-                energy_supply_connection,
-            );
-
             appliance_data.events.replace(vec![]);
             let event_appliance_gains = EventApplianceGains::new(
                 energy_supply_connection.clone(),
                 &simtime.iter(),
                 &appliance_data,
                 total_floor_area,
-                Some(smart_control.clone()),
+                Some(smart_control_for_process_event),
             )
             .unwrap();
 
