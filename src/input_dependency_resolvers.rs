69ee262a00efc52c0794bfb9f93c2348fea1f56f
rachowell <rachel.howell@madetech.com>
2026-09-25 16:34:13 +0100

    EC-1597 add battery related dependency resolving

--- src/input_dependency_resolvers.rs
+++ src/input_dependency_resolvers.rs
@@ -1,6 +1,7 @@
-use crate::input::PreHeatedWaterSourceDetails;
+use crate::input::{HeatBatteryPcmChargingSource, PreHeatedWaterSourceDetails};
 use arcstr::ArcStr;
 use indexmap::IndexMap;
+use itertools::Itertools;
 use petgraph::algo::toposort;
 use petgraph::Graph;
 use thiserror::Error;
@@ -33,7 +34,7 @@ pub(crate) fn build_preheated_water_source_dependency_graph(
 /// Returns a list of names in dependency order (dependencies first).
 fn topological_sort<T: Clone>(
     dependency_graph: &Graph<T, T>,
-    error_context: String,
+    error_context: ArcStr,
 ) -> Result<Vec<T>, CircularDependencyError> {
     match toposort(dependency_graph, None) {
         Ok(ordered_nodes) => Ok(ordered_nodes
@@ -51,9 +52,45 @@ pub(crate) fn topological_sort_preheated_water_sources<T: Clone>(
     topological_sort(dependency_graph, "PreHeatedWaterSource".into())
 }
 
+/// Build a dependency graph for PCM heat battery hydronic charging.
+pub(crate) fn build_heat_battery_charging_dependency_graph(
+    pcm_hydronic_charging_pending: &IndexMap<ArcStr, (ArcStr, HeatBatteryPcmChargingSource)>, // TODO sort out this type
+) -> Graph<ArcStr, ArcStr> {
+    let mut graph = Graph::<ArcStr, ArcStr>::new();
+    let mut nodes: IndexMap<ArcStr, _> = IndexMap::new();
+
+    for name in pcm_hydronic_charging_pending.keys() {
+        let node_index = graph.add_node(name.into());
+        nodes.insert(name.into(), node_index);
+    }
+
+    let mut edges = Vec::new();
+
+    for (battery_name, src_data) in pcm_hydronic_charging_pending.iter() {
+        let charging_source_name = &src_data.0;
+        if pcm_hydronic_charging_pending
+            .keys()
+            .contains(&charging_source_name)
+        {
+            edges.push((nodes[charging_source_name], nodes[battery_name]));
+        }
+    }
+
+    graph.extend_with_edges(&edges);
+
+    graph
+}
+
+/// Topological sort for heat battery charging dependencies.
+pub(crate) fn topological_sort_heat_battery_charging<T: Clone>(
+    dependency_graph: &Graph<T, T>,
+) -> Result<Vec<T>, CircularDependencyError> {
+    topological_sort(dependency_graph, "heat battery charging".into())
+}
+
 #[derive(Debug, Error, PartialEq)]
 #[error("Circular dependency detected in {0}")]
-pub(crate) struct CircularDependencyError(String);
+pub(crate) struct CircularDependencyError(ArcStr);
 
 #[cfg(test)]
 mod tests {
